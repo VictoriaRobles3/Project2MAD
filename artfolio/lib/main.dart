735def1c9d6e1613dd23b081f2aa30e86d4a4855
VictoriaRobles3 <traroblx@gmail.com>
2024-04-16 13:02:58 -0400

    Configure firebase project, add dependencies to .yaml file.

--- artfolio/lib/main.dart
+++ artfolio/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
