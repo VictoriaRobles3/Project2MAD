a74f1c02d169933b9239ea559b241de28000afa6
VictoriaRobles3 <traroblx@gmail.com>
2024-04-23 20:40:30 -0400

    Add change pfp feature.

--- artfolio/lib/main.dart
+++ artfolio/lib/main.dart
@@ -1,14 +1,15 @@
-import 'package:artfolio/homePage.dart';
 import 'package:artfolio/splashScreen.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() async {
+Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAppCheck.instance.activate();
   runApp(MainApp());
 }
 
@@ -19,11 +20,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: HomePage(),
-    /*  home: SplashScreen(
+      //  home: HomePage(),
+      home: SplashScreen(
         backgroundColor: Color.fromARGB(255, 244, 248, 255),
         duration: Duration(seconds: 2),
-      ), */ 
+      ), 
     );
   }
 }
--- artfolio/lib/profile.dart
+++ artfolio/lib/profile.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,6 +16,8 @@ class _ProfilePageState extends State<ProfilePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   User? _currentUser;
   DocumentSnapshot? _userData;
+
+  String imageURL = '';
 
   @override
   void initState() {
@@ -33,9 +38,33 @@ class _ProfilePageState extends State<ProfilePage> {
     if (docSnapshot.exists) {
       setState(() {
         _userData = docSnapshot;
+        imageURL = _userData!['profilePictureUrl'] ?? '';
+
       });
     } else {
       print('User data not found.');
+    }
+  }
+
+  Future<void> _uploadImage(File file) async {
+    String uniqueID = DateTime.now().millisecondsSinceEpoch.toString();
+
+    Reference referenceRoot = FirebaseStorage.instance.ref();
+    Reference referenceDirImages = referenceRoot.child('profileImages');
+    Reference referenceImageToUpload = referenceDirImages.child(uniqueID);
+
+    try {
+      await referenceImageToUpload.putFile(file);
+      imageURL = await referenceImageToUpload.getDownloadURL();
+      await FirebaseFirestore.instance.collection('users').doc(_currentUser!.uid).update({
+        'profilePictureUrl': imageURL,
+      });
+      setState(() {
+        
+      });
+      print('Profile picture uploaded and URL saved to database successfully!');
+    } catch (e) {
+      print('Error uploading profile picture: $e');
     }
   }
 
@@ -46,57 +75,84 @@ class _ProfilePageState extends State<ProfilePage> {
         title: Text('Profile'),
       ),
       body: _userData != null
-          ? Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundColor: Colors.grey,
-                    child: Text(
-                      'Add Profile Picture',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
+    ? Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.grey,
+                  backgroundImage:
+                      imageURL.isNotEmpty ? NetworkImage(imageURL) : null,
+                  child: imageURL.isEmpty
+                      ? IconButton(
+                          icon: Icon(Icons.add_a_photo),
+                          onPressed: () async {
+                            ImagePicker imagePicker = ImagePicker();
+                            XFile? file = await imagePicker.pickImage(
+                                source: ImageSource.gallery);
+                            print('${file?.path}');
+
+                            if (file == null) return;
+                            await _uploadImage(File(file.path));
+                          },
+                        )
+                      : null,
+                ),
+                SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 25),
+                    Text(
+                      '${_userData!['firstName']} ${_userData!['lastName']}',
+                      style: GoogleFonts.jetBrainsMono(
+                        textStyle: TextStyle(fontSize: 15),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 50),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 25),
+                    SizedBox(height: 10),
+                    Text(
+                      'Birthday: ${DateFormat('dd-MM-yyyy').format((_userData!['dob'] as Timestamp).toDate())}',
+                      style: GoogleFonts.jetBrainsMono(
+                        textStyle: TextStyle(fontSize: 13),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'User since: ${DateFormat('dd-MM-yyyy').format((_userData!['registrationDatetime'] as Timestamp).toDate())}',
+                      style: GoogleFonts.jetBrainsMono(
+                        textStyle: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                  ],
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                ImagePicker imagePicker = ImagePicker();
+                XFile? file = await imagePicker.pickImage(
+                    source: ImageSource.gallery);
+                print('${file?.path}');
 
-                      Text(
-                        '${_userData!['firstName']} ${_userData!['lastName']}',
-                        style: GoogleFonts.jetBrainsMono(
-                          textStyle: TextStyle(fontSize: 15),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Birthday: ${DateFormat('dd-MM-yyyy').format((_userData!['dob'] as Timestamp).toDate())}',
-                        style: GoogleFonts.jetBrainsMono(
-                          textStyle: TextStyle(fontSize: 13),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'User since: ${DateFormat('dd-MM-yyyy').format((_userData!['registrationDatetime'] as Timestamp).toDate())}',
-                        style: GoogleFonts.jetBrainsMono(
-                          textStyle: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          : _currentUser != null
-              ? Center(child: CircularProgressIndicator())
-              : Text(''),
+                if (file == null) return;
+                await _uploadImage(File(file.path));
+              },
+              child: Text('Update Profile Picture'),
+            ),
+          ],
+        ),
+      )
+    : _currentUser != null
+        ? Center(child: CircularProgressIndicator())
+        : Text(''),
+
     );
   }
 }
+
