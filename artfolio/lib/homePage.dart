809a4a901d4e124f151f5bd903c86833e4ad7893
VictoriaRobles3 <traroblx@gmail.com>
2024-04-25 20:04:48 -0400

    Add homepage feed.

--- artfolio/lib/homePage.dart
+++ artfolio/lib/homePage.dart
@@ -1,70 +1,15 @@
 import 'package:artfolio/addBoards.dart';
 import 'package:artfolio/menu.dart';
-import 'package:artfolio/profile.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  User? _currentUser;
-  DocumentSnapshot? _userData;
-
-  String imageURL = '';
-  List<Map<String, dynamic>> _boards = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentUser();
-    _fetchUserData();
-    _fetchBoards();
-  }
-
-  void _getCurrentUser() async {
-    _currentUser = _auth.currentUser;
-    if (_currentUser != null) {
-      _fetchUserData();
-    }
-  }
-
-  void _fetchUserData() async {
-    final DocumentSnapshot docSnapshot =
-        await FirebaseFirestore.instance.collection('users').doc(_currentUser!.uid).get();
-    if (docSnapshot.exists) {
-      setState(() {
-        _userData = docSnapshot;
-        imageURL = _userData!['profilePictureUrl'] ?? '';
-      });
-    } else {
-      print('User data not found.');
-    }
-  }
-
-  Future<void> _fetchBoards() async {
-    final QuerySnapshot snapshot = await FirebaseFirestore.instance
-        .collection('boards')
-        .orderBy('timeOfPost', descending: true)
-        .get();
-
-    setState(() {
-      _boards = snapshot.docs.map((doc) => {
-        'id': doc.id,
-        ...doc.data() as Map<String, dynamic>,
-      }).toList();
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Home'),
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {
@@ -75,110 +20,69 @@ class _HomePageState extends State<HomePage> {
           },
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Home page",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ProfilePage()),
-                  );
-                },
-                child: Container(
-                  width: 150,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "View Profile",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('boards').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final boards = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: boards.length,
+            itemBuilder: (context, index) {
+              final board = boards[index];
+              final boardURL = board.get('boardURL');
+              final description = board.get('description');
+              final firstName = board.get('Fname');
+              final lastName = board.get('Lname');
+              final timeOfBoard = board.get('timeOfBoard');
+
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        boardURL,
+                        width: double.infinity,
+                        height: 200,
+                        fit: BoxFit.cover,
                       ),
-                    ),
+                      SizedBox(height: 16),
+                      Text(
+                        '$firstName $lastName',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(description),
+                      SizedBox(height: 8),
+                      Text(
+                        'Posted at: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(timeOfBoard.toDate())}',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _boards.length,
-                  itemBuilder: (context, index) {
-                    final post = _boards[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Stack(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              AspectRatio(
-                                aspectRatio: 1.0,
-                                child: Image.network(
-                                  post['boardURL'],
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          '${post['firstName']} ${post['lastName']}',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        SizedBox(height: 4),
-                                        Text(post['description']),
-                                        SizedBox(height: 4),
-                                        Text(
-                                          DateFormat('yyyy-MM-dd HH:mm').format((post['timeOfPost'] as Timestamp).toDate()),
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-              _currentUser != null
-                  ? Center(child: CircularProgressIndicator())
-                  : Text(''),
-            ],
-          ),
-        ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: Align(
         alignment: Alignment.bottomCenter,
